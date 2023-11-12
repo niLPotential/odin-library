0a348f0c3215ef1cae0fb71fe2c3abf76e5432c6
niLPotential <niLPotential@proton.me>
2023-11-12 15:11:51 +0900

    Diplay library as div

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+const libraryArr = [];
+
+const testBook = new Book("Title", "Author", 99, false);
+addBookToLibrary(testBook);
+
+const testBtn = document.querySelector("#testBtn");
+testBtn.onclick = displayLibrary;
+
+const library = document.querySelector("#library");
 const newBookBtn = document.querySelector("#newBookBtn");
 const addBookDialog = document.querySelector("#addBookDialog");
+const addBookBtn = addBookDialog.querySelector("#addBookBtn");
 
 newBookBtn.onclick = () => addBookDialog.showModal();
-
-const library = [];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -18,14 +26,26 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(book) {
-  library.push(book);
+  libraryArr.push(book);
 }
 
-function displayBooks() {
-  for (const book of library) {
-    console.log(book.info());
+function displayLibrary() {
+  for (const book of libraryArr) {
+    library.appendChild(addBookAsElement(book));
   }
 }
 
-const testBook = new Book("Title", "Author", 99, false);
-addBookToLibrary(testBook);
+function addBookAsElement(book) {
+  const div = document.createElement("div");
+  div.appendChild(createDivWithText(book.title));
+  div.appendChild(createDivWithText(book.author));
+  div.appendChild(createDivWithText(book.pages));
+  div.appendChild(createDivWithText(book.read));
+  return div;
+}
+
+function createDivWithText(str) {
+  const div = document.createElement("div");
+  div.innerText = str;
+  return div;
+}
