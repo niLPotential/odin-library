2f64c86ee6778360a7c057fe9ce06e28d53ecba4
niLPotential <niLPotential@proton.me>
2023-11-21 20:40:52 +0900

    Change library to list

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-const libraryArr = [];
-
-const testBook = new Book("Title", "Author", 99, false);
-addBookToLibrary(testBook);
-
-const testBtn = document.querySelector("#testBtn");
-testBtn.onclick = displayLibrary;
-
-const library = document.querySelector("#library");
-const newBookBtn = document.querySelector("#newBookBtn");
-const addBookDialog = document.querySelector("#addBookDialog");
-const addBookBtn = addBookDialog.querySelector("#addBookBtn");
-
-newBookBtn.onclick = () => addBookDialog.showModal();
-
-addBookDialog.onclose = (e) => {
-  console.log(addBookDialog.returnValue);
-};
-
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -29,23 +10,59 @@ function Book(title, author, pages, read) {
   };
 }
 
+const libraryArr = [];
+
+const testBook = new Book("Title", "Author", 99, false);
+addBookToLibrary(testBook);
+
+const testBtn = document.querySelector("#testBtn");
+testBtn.onclick = displayLibrary;
+
+const libraryElem = document.querySelector("#library");
+const newBookBtn = document.querySelector("#newBookBtn");
+const addBookDialog = document.querySelector("#addBookDialog");
+const addBookForm = addBookDialog.querySelector("form");
+
+const addBookBtn = addBookDialog.querySelector("#addBookBtn");
+
+newBookBtn.onclick = () => addBookDialog.showModal();
+
+addBookDialog.onclose = (e) => {
+  if (addBookDialog.returnValue === "submit") {
+    const data = new FormData(addBookForm);
+    const newBook = newBookFromData(data);
+    addBookToLibrary(newBook);
+  }
+  addBookForm.reset();
+};
+
+function newBookFromData(data) {
+  const title = data.get("title");
+  const author = data.get("author");
+  const pages = data.get("pages");
+  const read = data.get("read");
+
+  return new Book(title, author, pages, read);
+}
+
 function addBookToLibrary(book) {
   libraryArr.push(book);
 }
 
 function displayLibrary() {
+  libraryElem.innerHTML = "";
   for (const book of libraryArr) {
-    library.appendChild(addBookAsElement(book));
+    libraryElem.appendChild(addBookAsElement(book));
   }
 }
 
 function addBookAsElement(book) {
-  const div = document.createElement("div");
-  div.appendChild(createDivWithText(book.title));
-  div.appendChild(createDivWithText(book.author));
-  div.appendChild(createDivWithText(book.pages));
-  div.appendChild(createDivWithText(book.read));
-  return div;
+  const li = document.createElement("li");
+  li.appendChild(createDivWithText(book.title));
+  li.appendChild(createDivWithText(book.author));
+  li.appendChild(createDivWithText(book.pages));
+  li.appendChild(createDivWithText(book.read));
+  return li;
 }
 
 function createDivWithText(str) {
